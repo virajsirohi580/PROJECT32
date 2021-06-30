ecc1173c9d485e4ae434f452e73510cefe1a9e35
virajsirohi580 <77614067+virajsirohi580@users.noreply.github.com>
2021-06-30 16:58:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,94 @@
+  
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ="sunrise1.png";
+var hour;
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+
+
+    Engine.update(engine);
+
+    fill("black")
+textSize(30)
+    // write code to display time in correct format here
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+       }else if(hour==0){
+         text("Time : 12 AM",100,100);
+       }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+       }
+
+
+}
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+
+    //change the data in JSON format
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    
+    // write code slice the datetime
+    hour = datetime.slice(11,13);
+
+    if(hour>=04 && hour<=06){
+        bg = "sunrise1.png";
+    }
+    
+    else if(hour>=06 && hour<=08){
+        bg = "sunrise2.png";
+    }
+
+    else if(hour>=08 && hour<=11){
+        bg = "sunrise3.png";
+    }
+    else if(hour>=11 && hour<=13){
+        bg = "sunrise4.png";
+    }
+    else if(hour>=13 && hour<=15){
+        bg = "sunrise5.png";
+    }
+    else if(hour>=15 && hour<=17){
+        bg = "sunrise6.png";
+    }
+    else if(hour>=17 && hour<=19){
+        bg = "sunrise2.png";
+    }
+
+    else if(hour>=19 && hour==21){
+        bg = "sunset10.png";
+    }
+    
+    else if(hour>=21 && hour==24){
+        bg = "sunset11.png";
+    }
+    
+    else{
+        bg = "sunset12.png";
+    }
+backgroundImg = loadImage(bg);
+
+}
